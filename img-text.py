47f3c8b0b59dbb524115bc5f4d1292fcac8d1fc4
Jignas Paturu <86356085+JignasP@users.noreply.github.com>
2023-03-27 16:43:56 +0530

    Update img-text.py

--- img-text.py
+++ img-text.py
@@ -1,20 +1,27 @@
-
+import argparse
 from PIL import Image
 
 class code :
 
+    descStr = "This program converts an image into ASCII art."
+    parser = argparse.ArgumentParser(description=descStr)
+    parser.add_argument('--file', dest='filename', required=True)
+    parser.add_argument('--width', dest='width', required=False)
 
-    filename = "sad.jpg"
+    args = parser.parse_args()
+    filename = args.filename
+
+    new_width = 80
+    if args.width:
+        new_width = float(args.scale)
 
     img = Image.open(filename)
     ASCII_CHARS = "@#S%?*+;:,."
-    new_width=120
+
     width, height = img.size
     ratio = height / width
     new_height = int(new_width * ratio)
     img = img.resize((new_width, new_height)).convert('L')
-
-
 
     txt = ""
     for y in range(img.size[1]):
